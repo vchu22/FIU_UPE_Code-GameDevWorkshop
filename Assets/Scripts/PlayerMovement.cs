b6bf9a80ad5eef9ef337492a7145aed4af234de5
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2021-05-05 09:42:29 -0400

    set max speed for player/turn animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator animator;
     public float maxSpeed, jumpHeight;
 
     void Update()
     {
+      animator.SetFloat("walkingSpeed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
       if (Input.GetKeyDown(KeyCode.W)) {
         GetComponent<Rigidbody2D>().velocity += new Vector2(0, jumpHeight);
       }
       if (Input.GetKey(KeyCode.A)) {
         // Goes left
         GetComponent<Rigidbody2D>().AddForce(new Vector2(-maxSpeed/4, 0), ForceMode2D.Force);
+        GetComponent<Rigidbody2D>().transform.rotation = new Quaternion(0, 180, 0, 9);
       }
       if (Input.GetKey(KeyCode.D)) {
         // Goes right
         GetComponent<Rigidbody2D>().AddForce(new Vector2(+maxSpeed/4, 0), ForceMode2D.Force);
+        GetComponent<Rigidbody2D>().transform.rotation = new Quaternion(0, 0, 0, 9);
+      }
+    }
+
+    void FixedUpdate() 
+    {
+      Rigidbody2D player = GetComponent<Rigidbody2D>();
+      if (player.velocity.x > maxSpeed) {
+        // CAP
+        player.velocity = new Vector2(maxSpeed, player.velocity.x);
+      } else if (player.velocity.x < -maxSpeed) {
+        player.velocity = new Vector2(maxSpeed, player.velocity.y);
       }
     }
 }
